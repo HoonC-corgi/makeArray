3de5d6ce1b36f35f5c16d0befb874d74cc30d36a
HoonC-corgi <tgh7544@naver.com>
2022-11-15 17:39:52 +0900

    make Array apply forEach

--- src/main/java/apply_forEach/Main.java
+++ src/main/java/apply_forEach/Main.java
@@ -10,7 +10,6 @@ public class Main {
         for(int x: intArray) {
             System.out.print(x);
         }
-
         int max = intArray[0];
         for(int x: intArray) {
             if(x>max) {
